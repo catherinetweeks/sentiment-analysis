67664059625b63b8832daf7635665fb919407fd5
Catherine Weeks <catherinetweeks@gmail.com>
2024-09-22 22:48:01 -0400

    Worked on environment and imports

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
